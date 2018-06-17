fe8f8ca3b6e8cf6b9d5032a9b54bbbdf9222d2e9
yifatBi <yifat@3baysover.com>
2018-06-17 15:36:37 +0300

    implement cookie

--- grails-app/assets/javascripts/application.js
+++ grails-app/assets/javascripts/application.js
@@ -8,7 +8,18 @@
 //= require jquery-2.2.0.min
 //= require bootstrap
 //= require_self
-
+   function setCookie(cname, cvalue, exdays) {
+        var d = new Date();
+        d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+        var expires = "expires=" + d.toUTCString();
+        document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+    }
+    setCookie('user-id', Math.floor((Math.random() * 10) + 1), 3)
+    function getCookie(name) {
+        var re = new RegExp(name + "=([^;]+)");
+        var value = re.exec(document.cookie);
+        return (value != null) ? unescape(value[1]) : null;
+    }
 if (typeof jQuery !== 'undefined') {
     (function($) {
         $(document).ajaxStart(function() {
